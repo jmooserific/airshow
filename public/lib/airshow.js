7d6da3d1238b7ac28d49c064a21f8c71631dcc72
John Moose <john@mooserific.org>
2013-05-01 10:33:11 -0400

    Cleaned up trailing spaces

--- public/lib/airshow.js
+++ public/lib/airshow.js
@@ -2,13 +2,13 @@ function showLightbox() {
     $('#lightbox .flipper').css( "-webkit-transform","rotateY(-90deg)" );
     $('#lightbox .flipper').css( "-moz-transform","rotateY(-90deg)" );
     $('#lightbox .flipper').css( "transform","rotateY(-90deg)" );
-    
+
     $('#lightbox').fadeIn();
-    
+
     $('#lightbox .flipper').css( "-webkit-transform","rotateY(0deg)" );
     $('#lightbox .flipper').css( "-moz-transform","rotateY(0deg)" );
     $('#lightbox .flipper').css( "transform","rotateY(0deg)" );
-    
+
     $('#lightbox .front').click(function(event) {
         $('#lightbox .flipper').css( "-webkit-transform","rotateY(180deg)" );
         $('#lightbox .flipper').css( "-moz-transform","rotateY(180deg)" );
@@ -20,7 +20,7 @@ function showLightbox() {
         $('#lightbox .flipper').css( "-webkit-transform","rotateY(0deg)" );
         $('#lightbox .flipper').css( "-moz-transform","rotateY(0deg)" );
         $('#lightbox .flipper').css( "transform","rotateY(0deg)" );
-        
+
         event.stopPropagation();
     });
 }
@@ -29,27 +29,27 @@ function resizeElements() {
     if ($('#lightbox').is(':visible') && $('#lightbox .front img')) {
 		var originalWidth = 800;
 		var originalHeight = 800;
-		
+
         if ($('#lightbox .front img').attr('data-width')) {
 			originalWidth = $('#lightbox .front img').attr('data-width');
 		} else {
 			originalWidth = $('#lightbox .front img').width();
 			$('#lightbox .front img').attr('data-width', originalWidth);
 		}
-		
+
 		if ($('#lightbox .front img').attr('data-height')) {
 			originalHeight = $('#lightbox .front img').attr('data-height');
 		} else {
 			originalHeight = $('#lightbox .front img').height();
 			$('#lightbox .front img').attr('data-height', originalHeight);
 		}
-		        
+
         var safeWidth = $(document).width() - 40;
         var safeHeight = $(document).height() - 40;
 
         var scaleWidth = originalWidth / safeWidth;
         var scaleHeight = originalHeight / safeHeight;
-        
+
         if (originalWidth / scaleWidth >= originalWidth && originalHeight / scaleHeight >= originalHeight) {
             $('.flip-container, .front, .back').width(originalWidth);
             $('.flip-container, .front, .back').height(originalHeight);
